7fddd8a115e61e2a11a54769826104ae49705958
TJ Yanagitsuru <yanagitsuru.taro@moneyforward.co.jp>
2025-11-15 23:17:57 +0900

    big edit. rename, normalize, type cast

--- spending_dbt/models/src/src_raw_spending.sql
+++ spending_dbt/models/src/src_raw_spending.sql
@@ -1,2 +1,122 @@
+    -- clean date
+    -- convert the date column to a date type
+
+with source as  (
+
+    select 
+        Timestamp,
+        transaction_type,
+        amount, 
+        payment_method,
+        payee,
+        item,
+        category, 
+        tags,
+        food_details,
+        hobby_details,
+        trip_details,
+        social,
+        store_type,
+        purchase_channel,
+        essentiality, 
+        recurrence_type,
+        value_rating,
+        Random_memos
+    from {{ source('raw_spending', 'expense_tracker') }}
+    
+), 
+
+renamed as (
+    select 
+        Timestamp as timestamp_raw,
+        transaction_type,
+        amount, 
+        payment_method,
+        payee,
+        item,
+        category, 
+        tags,
+        food_details,
+        hobby_details,
+        trip_details,
+        social,
+        store_type,
+        purchase_channel,
+        essentiality, 
+        recurrence_type,
+        value_rating,
+        Random_memos as notes
+    from source
+),
+
+normalized as (
+    select
+        -- to deal with 2025/10/20 format
+        case
+            -- YYYY/MM/DD (year-first)
+            when regexp_contains(timestamp_raw, r'^\d{4}/') then parse_datetime('%Y/%m/%d %H:%M:%S', concat(timestamp_raw, ' 00:00:00'))
+            -- M/D/YYYY or MM/DD/YYYY (month-first, no time)
+            when regexp_contains(timestamp_raw, r'^\d{1,2}/\d{1,2}/\d{4}$') then parse_datetime('%m/%d/%Y %H:%M:%S', concat(timestamp_raw, ' 00:00:00'))
+            -- Has time portion (MM/DD/YYYY HH:MM:SS)
+            when regexp_contains(timestamp_raw, r'\d{1,2}:\d{2}:\d{2}') then parse_datetime('%m/%d/%Y %H:%M:%S', timestamp_raw)
+            else NULL
+        end as timestamp_datetime,
+
+        --     when timestamp_raw not like '% %' then
+        --         concat(
+        --             substr(timestamp_raw, 6, 2), 
+        --             '/', 
+        --             substr(timestamp_raw, 9, 2),
+        --             '/', 
+        --             substr(timestamp_raw, 1, 4),
+        --             ' 00:00:00'
+        --         )
+        --     else timestamp_raw
+        -- end as timestamp_normalized,
+        transaction_type,
+        amount, 
+        payment_method,
+        payee,
+        item,
+        category, 
+        tags,
+        food_details,
+        hobby_details,
+        trip_details,
+        social,
+        store_type,
+        purchase_channel,
+        essentiality, 
+        recurrence_type,
+        value_rating,
+        notes
+    from renamed
+),
+
+type_cast as (
+    select 
+        timestamp_datetime,
+        cast(transaction_type as string) as transaction_type,
+        cast(amount as int64) as amount, 
+        cast(payment_method as string) as payment_method,
+        cast(payee as string) as payee,
+        cast(item as string) as item,
+        cast(category as string) as category, 
+        cast(tags as string) as tags,
+        cast(food_details as string) as food_details,
+        cast(hobby_details as string) as hobby_details,
+        cast(trip_details as string) as trip_details,
+        cast(social as string) as social,
+        cast(store_type as string) as store_type,
+        cast(purchase_channel as string) as purchase_channel,
+        cast(essentiality as string) as essentiality,
+        cast(recurrence_type as string) as recurrence_type,
+        cast(value_rating as int64) as value_rating,
+        cast(notes as string) as notes
+    from normalized
+)
+
 select * 
-from {{ source('raw_spending', 'expense_tracker_responses') }}
+    from type_cast
+
+
