d393949e99df8ce6dea60a982f5ccadb1c2398c3
TJ Yanagitsuru <yanagitsuru.taro@moneyforward.co.jp>
2025-11-16 00:31:05 +0900

    income and expense columns

--- spending_dbt/models/stg/stg_add_derived_columns.sql
+++ spending_dbt/models/stg/stg_add_derived_columns.sql
@@ -1,9 +1,38 @@
-select 
-    *
     -- case when transaction_type = '💰income💰' then amount else 0 end as income,
     -- case when transaction_type = 'expense' then amount else 0 end as expense
-from {{ ref('src_raw_spending') }}
+    -- add date, month and day of week, is weekend columns
 
--- separate out the expenses and income into plus minus in another column
--- when transaction_type is 'income', the amount is positive
--- when transaction_type is 'expense', the amount is negative
+
+
+with 
+
+src as (
+    select * 
+    from {{ ref('src_raw_spending') }}
+), 
+
+income_expense as (
+    select 
+        timestamp_datetime,
+        amount,
+        case when transaction_type = '💰income💰' then amount else 0 end as income,
+        case when transaction_type = 'expense' then -amount else 0 end as expense,
+        payment_method,
+        payee,
+        item,
+        category,
+        tags,
+        food_details,
+        hobby_details,
+        trip_details,
+        social,
+        store_type,
+        purchase_channel,
+        essentiality,
+        recurrence_type,
+        value_rating,
+        notes
+    from src
+)
+
+select * from income_expense
