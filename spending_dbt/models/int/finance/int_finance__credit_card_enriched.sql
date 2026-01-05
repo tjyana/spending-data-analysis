1af119c3561be6a888fcdc8a038e154474d91a88
TJ Yanagitsuru <yanagitsuru.taro@moneyforward.co.jp>
2026-01-06 02:45:33 +0900

    fix issue where it was picking up legacy cc fill-ins for new data

--- spending_dbt/models/int/finance/int_finance__credit_card_enriched.sql
+++ spending_dbt/models/int/finance/int_finance__credit_card_enriched.sql
@@ -13,42 +13,42 @@ thb as (
 select
   -- 1) identifiers / timestamps
   coalesce(thb.id_key, cc.id_key) as id_key,
-  cc.timestamp_datetime as timestamp_datetime,
-  cc.transaction_date as transaction_date,
-  cc.transaction_month as transaction_month,
-  cc.transaction_month_year as transaction_month_year,
-  cc.transaction_day_of_week as transaction_day_of_week,
-  cc.is_weekend as is_weekend,
-  coalesce(cast(cc.amount as numeric), cast(thb.JPY as numeric)) as amount, -- doublecheck
-  cc.transaction_type as transaction_type,
-  cc.income as income,
-  cc.expense as expense,
-  cc.payment_method as payment_method,
-  cc.payment_method_complete as payment_method_complete,
+  cc.timestamp_datetime as timestamp_datetime, -- processed in cc
+  cc.transaction_date as transaction_date, -- processed in cc
+  cc.transaction_month as transaction_month, -- processed in cc
+  cc.transaction_month_year as transaction_month_year, -- processed in cc
+  cc.transaction_day_of_week as transaction_day_of_week, -- processed in cc
+  cc.is_weekend as is_weekend, -- processed in cc
+  coalesce(cast(cc.amount as numeric), cast(thb.JPY as numeric)) as amount, -- we want the cc amount here
+  cc.transaction_type as transaction_type, -- keep cc value
+  cc.income as income, -- keep cc value
+  cc.expense as expense, -- keep cc value
+  coalesce(thb.payment_method, cc.payment_method) as payment_method,
+  coalesce(thb.payment_method, cc.payment_method_complete) as payment_method_complete,
   coalesce(thb.payee, cc.payee) as payee,
-  cc.payee_standardized as payee_standardized, -- fix later
+  coalesce(thb.payee, cc.payee_standardized) as payee_standardized,
   coalesce(thb.item, cc.item) as item, 
   coalesce(thb.category, cc.category) as category,
-  cc.category_standardized as category_standardized, -- fix later
-  cc.category_complete as category_complete, -- fix later
+  coalesce(thb.category, cc.category_standardized) as category_standardized, 
+  coalesce(thb.category, cc.category_complete) as category_complete, 
   coalesce(thb.tags, cc.tags) as tags,
-  cc.tags_complete as tags_complete, -- fix later
+  coalesce(thb.tags, cc.tags_complete) as tags_complete, 
   coalesce(thb.food_details, cc.food_details) as food_details,
   coalesce(thb.hobby_details, cc.hobby_details) as hobby_details,
   coalesce(thb.trip_details, cc.trip_details) as trip_details,
   coalesce(thb.social, cc.social) as social,
-  cc.social_complete as social_complete, -- fix later
+  coalesce(thb.social, cc.social_complete) as social_complete, 
   coalesce(thb.store_type, cc.store_type) as store_type, 
-  cc.store_type_standardized as store_type_standardized, -- fix later
-  cc.store_type_complete as store_type_complete, -- fix later
+  coalesce(thb.store_type, cc.store_type_standardized) as store_type_standardized, 
+  coalesce(thb.store_type, cc.store_type_complete) as store_type_complete, 
   coalesce(thb.purchase_channel, cc.purchase_channel) as purchase_channel,
-  cc.purchase_channel_complete as purchase_channel_complete, -- fix later
+  coalesce(thb.purchase_channel, cc.purchase_channel_complete) as purchase_channel_complete, 
   coalesce(thb.essentiality, cc.essentiality) as essentiality,
-  cc.essentiality_complete as essentiality_complete, -- fix later
+  coalesce(thb.essentiality, cc.essentiality_complete) as essentiality_complete,
   coalesce(thb.recurrence_type, cc.recurrence_type) as recurrence_type,
-  cc.recurrence_type_complete as recurrence_type_complete, -- fix later
+  coalesce(thb.recurrence_type, cc.recurrence_type_complete) as recurrence_type_complete, 
   coalesce(thb.value_rating, cc.value_rating) as value_rating,
-  cc.value_rating_complete as value_rating_complete, -- fix later
+  coalesce(thb.value_rating, cc.value_rating_complete) as value_rating_complete, 
   coalesce(thb.notes, cc.notes) as notes,
   coalesce(thb.anomaly, cc.anomaly) as anomaly,
   coalesce(thb.source_system, cc.source_system) as source_system
