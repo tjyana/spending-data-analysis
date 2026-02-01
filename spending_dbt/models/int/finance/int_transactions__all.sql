4bc09d55e4cddea6b07cb2d869786a7b2d3c8de2
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2026-02-01 12:21:20 +0900

    add safe cast to recurrence type to try and get around error for now

--- spending_dbt/models/int/finance/int_transactions__all.sql
+++ spending_dbt/models/int/finance/int_transactions__all.sql
@@ -1,14 +1,61 @@
 -- join credit card statements with expense tracker data
 
+with all as (
+
 select *
 from {{ ref("stg_expense_tracker__transactions_complete") }}
 
-union all 
+union all
 
-select * 
+select *
 from {{ ref("int_transactions__credit_card_enriched")}}
 
 union all
 
 select *
 from {{ ref("int_transactions__thb_cash")}}
+
+)
+
+select
+  id_key,
+  timestamp_datetime,
+  transaction_date,
+  transaction_month,
+  transaction_month_year,
+  transaction_day_of_week,
+  is_weekend,
+  amount,
+  transaction_type,
+  income,
+  expense,
+  payment_method,
+  payment_method_complete,
+  payee,
+  payee_standardized, -- fix later
+  item,
+  category,
+  category_standardized, -- fix later
+  category_complete, -- fix later
+  tags,
+  tags_complete, -- fix later
+  food_details,
+  hobby_details,
+  trip_details,
+  social,
+  social_complete, -- fix later
+  store_type,
+  store_type_standardized, -- fix later
+  store_type_complete, -- fix later
+  purchase_channel,
+  purchase_channel_complete, -- fix later
+  essentiality,
+  essentiality_complete, -- fix later
+  safe_cast(recurrence_type as int64) as recurrence_type,
+  recurrence_type_complete, -- fix later
+  value_rating,
+  value_rating_complete, -- fix later
+  notes,
+  anomaly,
+  source_system
+from all
